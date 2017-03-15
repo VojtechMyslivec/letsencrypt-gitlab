1cd599c62999ee72e5ab531e7b818993eff416c5
Vojtech Myslivec <vojtech@xmyslivec.cz>
2017-03-15 10:37:56 +0100

    Refactor script code info functions

--- letsencrypt_wrapper.sh
+++ letsencrypt_wrapper.sh
@@ -7,6 +7,7 @@
 set -o pipefail
 set -o nounset
 
+# variables ======================================
 SCRIPTNAME=${0##*/}
 SCRIPTDIR=${0%/*}
 
@@ -39,7 +40,8 @@ letsencrypt="/opt/letsencrypt/letsencrypt-auto"
 # DANGEROUS! it will be expanded without quotes
 letsencrypt_extra_args=""
 
-# functions --------------------------------------
+# functions ======================================
+# messages ---------------------------------------
 message() {
     echo "$SCRIPTNAME[$1]: $2" >&2
 }
@@ -62,12 +64,8 @@ error() {
       message "err" "$*"
 }
 
-# usage ------------------------------------------
-source "$SCRIPTDIR/letsencrypt_wrapper.conf" || {
-    error "Could not source config file."
-    exit 1
-}
-
+# script functions -------------------------------
+usage() {
 [ $# -le 1 ] || {
     echo "$USAGE" >&2
     exit 1
@@ -99,16 +97,9 @@ case "$log_level" in
         exit 1
         ;;
 esac
-
-# needed for dummy webserver
-cd "$webroot_path" || {
-    error "Can not change directory to '$webroot_path'"
-    exit 1
 }
 
-
-# main -------------------------------------------
-
+obtain_gitlab_cert() {
 # obtain certificate for gitlab – via webroot method
 info "Obtaining Gitlab certificate" >&2
 "$letsencrypt" certonly \
@@ -118,36 +109,56 @@ info "Obtaining Gitlab certificate" >&2
   --webroot --webroot-path "$webroot_path" \
   --expand --domains "$gitlab_domains" || {
     error "Failed to obtain certificate for Gitlab domains" >&2
-    exit 2
+    return 1
+}
 }
 
+restart_nginx() {
 info "Restarting nginx" >&2
 "$gitlab_sv" restart nginx > /dev/null || {
     error "Failed to restart nginx"
-    exit 3
+    return 1
+}
 }
 
-info "Stoping Pages"
-"$gitlab_sv" stop gitlab-pages > /dev/null || {
-    error "Failed to stop pages"
-    exit 3
+
+# Needs $1: start/stop
+pages_service() {
+    local action="$1"
+    if [ "$action" == "start" ]; then
+        info "Starting Pages"
+    elif [ "$action" == "stop" ]; then
+        info "Stoping Pages"
+    else
+        error "nginx_service(): argument must be 'start' or 'stop'"
+        return 1
+    fi
+    "$gitlab_sv" "$action" gitlab-pages > /dev/null || {
+        error "Failed to '$action' pages"
+        return 2
+    }
 }
 
+start_dummy_webserver() {
 info "Running dummy webserver"
 dummy_server_pidfile=$( mktemp )
 python3 -m http.server --bind "$pages_bind_ip" 80 &> /dev/null &
 echo "$!" > "$dummy_server_pidfile"
-
-sleep 2
-pgrep -P "$$" -F "$dummy_server_pidfile" python > /dev/null || {
-    error "Can not start dummy web server"
-    "$gitlab_sv" start gitlab-pages > /dev/null || {
-        error "Failed to start pages"
-        exit 3
-    }
-    exit 3
 }
 
+is_dummy_webserver_running() {
+pgrep -P "$$" -F "$dummy_server_pidfile" python > /dev/null
+}
+
+check_dummy_webserver() {
+is_dummy_webserver_running || {
+    error "Can not start dummy web server"
+    pages_service start
+    return 1
+}
+}
+
+obtain_pages_cert() {
 # obtain certificate for pages – via webroot method
 info "Obtaining Pages certificate" >&2
 "$letsencrypt" certonly \
@@ -157,19 +168,17 @@ info "Obtaining Pages certificate" >&2
   --webroot --webroot-path "$webroot_path" \
   --expand --domains "$pages_domains" || {
     error "Cannnot obtain certificate for Pages domains." >&2
-    "$gitlab_sv" start gitlab-pages > /dev/null || {
-        error "Failed to start pages"
-        exit 3
-    }
-    exit 2
+    return 1
+}
 }
 
+stop_dummy_webserver() {
 info "Stoping dummy webserver"
 pkill -P "$$" -F "$dummy_server_pidfile" python -term
 
 # in case it needs to be killed
 sleep 2
-pgrep -P "$$" -F "$dummy_server_pidfile" python > /dev/null && {
+is_dummy_webserver_running && {
     warning "Need to kill dummy webserver"
     pkill -P "$$" -F "$dummy_server_pidfile" python -kill
 }
@@ -177,9 +186,45 @@ pgrep -P "$$" -F "$dummy_server_pidfile" python > /dev/null && {
 rm "$dummy_server_pidfile" || {
     warning "Can not remove temporary pidfile '$dummy_server_pidfile'"
 }
-
-info "Starting Pages" >&2
-"$gitlab_sv" start gitlab-pages > /dev/null || {
-    error "Failed to start pages"
-    exit 3
 }
+
+
+# main ===========================================
+
+# needed for dummy webserver
+cd "$webroot_path" || {
+    error "Can not change directory to '$webroot_path'"
+    exit 1
+}
+
+source "$SCRIPTDIR/letsencrypt_wrapper.conf" || {
+    error "Could not source config file."
+    exit 1
+}
+
+usage "$@" || exit 1
+
+
+# obtain and deploy gitlab certificate -----------
+obtain_gitlab_cert || exit 2
+
+restart_nginx || exit 3
+
+
+# obtain and deploy pages certificate ------------
+pages_service stop || exit 3
+
+start_dummy_webserver || exit 3
+
+sleep 2
+check_dummy_webserver || exit 3
+
+obtain_pages_cert || {
+    stop_dummy_webserver || exit 3
+    pages_service start || exit 3
+    exit 2
+}
+
+stop_dummy_webserver || exit 3
+
+pages_service start || exit 3
